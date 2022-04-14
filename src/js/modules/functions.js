d73233b3daac6478008d220a24c70ca1a2755ac8
Danylo Kyrmach <tomahawk535@gmail.com>
2022-04-14 13:05:40 +0300

    add form validation

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -15,9 +15,7 @@ export function isWebp() {
 	});
 }
 
-
-
-/* роверка отображения на тачскрине */
+/* Проверка отображения на тачскрине */
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
@@ -38,9 +36,63 @@ if (isMobile.any()) {
 const upBtn = document.querySelector('.up__btn');
 upBtn.addEventListener("click", backToTop);
 
-	function backToTop(e) {
+	function backToTop() {
 			window.scrollTo({
 				top: 0,
 				behavior: "smooth"
 			});
 }
+
+
+/* Валидация формы */
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		if (error === 0) {
+
+		} else {
+			alert('Заполните все поля поля');
+		}
+	}
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	//Функция теста email
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+});
